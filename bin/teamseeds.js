0b62cc6f18f6b43f56ca1907691b170c2b1efec8
FranciscoGBento <f_m_bento@hotmail.com>
2022-05-13 16:31:02 +0100

    teams seeds

--- bin/teamseeds.js
+++ bin/teamseeds.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const Team = require("../models/Team.model");
 
-const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/library-project";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/iron-gp";
 
 mongoose
   .connect(MONGO_URI)
@@ -15,5 +14,95 @@ mongoose
     console.error("Error connecting to mongo: ", err);
   });
 
-
-
+const teams = [
+  {
+    name: "Oracle Red Bull Racing",
+    teamChief: "Christian Horner",
+    championships: 4,
+    country: "United Kingdom",
+    firstEntry: "1997",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/Red-Bull-Racing/_jcr_content/logo.img.jpg/1645620474276.jpg",
+  },
+  {
+    name: "Haas F1 Team",
+    teamChief: "Guenther Steiner",
+    championships: 0,
+    country: "United States",
+    firstEntry: "2016",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/Haas-F1-Team/_jcr_content/logo.img.jpg/1646210673011.jpg",
+  },
+  {
+    name: "BWT Alpine F1 Team",
+    teamChief: "Otmar Szafnauer",
+    championships: 2,
+    country: "United Kingdom",
+    firstEntry: "1986",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/Alpine/_jcr_content/logo.img.jpg/1645620884650.jpg",
+  },
+  {
+    name: "Mercedes-AMG Petronas F1 Team",
+    teamChief: "Toto Wolff",
+    championships: 8,
+    country: "United Kingdom",
+    firstEntry: "1970",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/Mercedes/_jcr_content/logo.img.jpg/1582638425211.jpg",
+  },
+  {
+    name: "Scuderia Ferrari",
+    teamChief: "Guenther Steiner",
+    championships: 16,
+    country: "Italy",
+    firstEntry: "1950",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/Ferrari/_jcr_content/logo.img.jpg/1521797474166.jpg",
+  },
+  {
+    name: "Scuderia AlphaTauri",
+    teamChief: "Franz Tost",
+    championships: 0,
+    country: "Italy",
+    firstEntry: "1985",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/AlphaTauri/_jcr_content/logo.img.jpg/1582650557134.jpg",
+  },
+  {
+    name: "Williams Racing",
+    teamChief: "Jost Capito",
+    championships: 9,
+    country: "United Kingdom",
+    firstEntry: "1978",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/Williams/_jcr_content/logo.img.jpg/1590743731407.jpg",
+  },
+  {
+    name: "McLaren F1 Team",
+    teamChief: "Andreas Seidl",
+    championships: 8,
+    country: "United Kingdom",
+    firstEntry: "1966",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/McLaren/_jcr_content/logo.img.jpg/1644831223911.jpg",
+  },
+  {
+    name: "Aston Martin Aramco Cognizant F1 Team",
+    teamChief: "Mike Krack",
+    championships: 0,
+    country: "United Kingdom",
+    firstEntry: "2018",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/Aston-Martin/_jcr_content/logo.img.jpg/1645620644658.jpg",
+  },
+  {
+    name: "Alfa Romeo F1 Team ORLEN",
+    teamChief: "Frédéric Vasseur",
+    championships: 0,
+    country: "Switzerland",
+    firstEntry: "1993",
+    imageUrl:
+      "https://www.formula1.com/content/fom-website/en/teams/Alfa-Romeo-Racing/_jcr_content/logo.img.png/1643019381208.png",
+  },
+];
