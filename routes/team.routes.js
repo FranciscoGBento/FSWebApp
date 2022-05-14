2219a9efc060cdb5aaadf74169137e2d26c301c0
FranciscoGBento <f_m_bento@hotmail.com>
2022-05-14 11:51:15 +0100

    Teams routes and views

--- routes/team.routes.js
+++ routes/team.routes.js
@@ -1,14 +1,30 @@
-const router = require('express').Router();
-const Team = require('../models/Team.model');
+const router = require("express").Router();
+const Team = require("../models/Team.model");
 
-router.get('/teams', (req, res, next) => {
-    Team.find({})
-      .then((teams) => {
-        res.render('teams/teams-list', { teams });
-      })
-      .catch((err) => next(err));
-  });
+router.get("/teams", (req, res, next) => {
+  Team.find({})
+    .then((teams) => {
+      res.render("teams/teams-list", { teams });
+    })
+    .catch((err) => next(err));
+});
 
+router.post("/teams", (req, res, next) => {
+  const { name, teamChief, championships, country, firstEntry, imageUrl } =
+    req.body;
 
+  Team.create({ name, teamChief, championships, country, firstEntry, imageUrl })
+    .then(() => res.redirect("/teams-list"))
+    .catch((err) => next(err));
+});
+
+router.get("/teams/:id", (req, res, next) => {
+  const { id } = req.params;
+  Team.findById(id)
+    .then((team) => {
+      res.render("team/teams-details", team);
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = router;
