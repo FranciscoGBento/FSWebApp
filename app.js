7ae38bb6b9085aa34d629d0277264eb6e8cd4229
FranciscoGBento <f_m_bento@hotmail.com>
2022-05-14 11:54:18 +0100

    add routes appjs

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use("/", index);
 const authRoutes = require("./routes/auth.routes");
 app.use("/", authRoutes);
 
+const teamsRoutes = require('./routes/team.routes');
+app.use('/', teamsRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
